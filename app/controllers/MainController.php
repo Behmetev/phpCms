47a1cadfd3945be94b5eb6ae9b8449a669d37b92
Nikita Behmetev <nikitabehmetev@gmail.com>
2022-06-30 14:38:05 +0300

    добавлен класс кеширования

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use phpCms\App;
+use phpCms\Cache;
 
 class MainController extends AppController
 {
@@ -10,7 +11,12 @@ class MainController extends AppController
     {
         $posts = \R::findAll('test');
         $this->setMeta(App::$app->getProperty('shopName'), " ... ", "00000");
-        $name = ['1234', '0000'];
+
+        // $name = "01010";
+        // $name = ['1234', '0000'];
+        // $cache = Cache::instance();
+        // $cache->set('test', $name);
+
         $this->set(compact('name', 'posts'));
     }
 }
