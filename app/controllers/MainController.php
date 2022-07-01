f5e05dea4ea2fb084ac977869fab1d4225ab5720
Nikita Behmetev <nikitabehmetev@gmail.com>
2022-07-01 14:59:48 +0300

    вывод данных на главную

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,6 +9,9 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+        $brands = \R::find('brand', 'LIMIT 3');
+        $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
         $this->setMeta(App::$app->getProperty('shopName'), " ... ", "00000");
+        $this->set(compact('brands', 'hits'));
     }
 }
